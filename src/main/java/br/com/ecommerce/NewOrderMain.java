51b86a6b95bb1638b59204701bf84f78141761ba
daytrevisan <dayane.trevisan@nubank.com.br>
2025-11-11 18:44:32 -0300

    feat: producer/consumer for new order and fraud detector

--- src/main/java/br/com/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/ecommerce/NewOrderMain.java
@@ -1,7 +1,6 @@
 package br.com.ecommerce;
 
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -9,17 +8,11 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public class NewOrderMain {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "121354654, 685315, 154445463515";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value); // topic, key, value
-        producer.send(record, (data, ex) -> {
-            if(ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("Sucesso enviando " + data.topic() + " ::: partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        }).get(); // mensagem enviada >>> registro
+        producer.send(record); // mensagem enviada >>> registro
     }
 
     private static Properties properties() {
