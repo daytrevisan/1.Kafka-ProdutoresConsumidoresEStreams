ffd5c749b71e111d16336f21cc470c3c54ff3f1c
daytrevisan <dayane.trevisan@nubank.com.br>
2025-11-11 18:42:17 -0300

    Starting with producers and consumers

---
+++ src/main/java/br/com/ecommerce/NewOrderMain.java
@@ -0,0 +1,33 @@
+package br.com.ecommerce;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class NewOrderMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "121354654, 685315, 154445463515";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value); // topic, key, value
+        producer.send(record, (data, ex) -> {
+            if(ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("Sucesso enviando " + data.topic() + " ::: partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        }).get(); // mensagem enviada >>> registro
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092"); // onde se conectar (chave, servidores/porta)
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // transformadores de string p/ bytes
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // value >>> mensagem
+
+        return properties;
+    }
+}
